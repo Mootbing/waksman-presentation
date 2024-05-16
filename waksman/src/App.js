de9779b5d4fd1c8214d0ebc31fb93009a3409b3e
Mootbing <50122069+Mootbing@users.noreply.github.com>
2024-05-15 22:46:59 -0400

    cook

--- waksman/src/App.js
+++ waksman/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       <body>
         <h1>E-Poster</h1>
-        <iframe style={{height: "80vh"}} src="https://drive.google.com/file/d/1xS7nI2856iT-HE6IGKPlNndDMkNuwVss/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe style={{height: "80vh"}} src="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/preview" width="100%" height="100%" allow="autoplay"></iframe>
         <h1>Videos</h1>
         <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/XDno7oiuduw?si=qo97KUr5rhiZOu16" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/5oEwQ401Ggw?si=4J-Tmkvu06pdE0sY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -19,9 +19,7 @@ function App() {
         <h1>Acknoweldgements</h1>
         <p>We would like to thank Ms. Wleklinski, Dr. Pierce, and Ms. Zanfardino of Rutgers Preparatory School for their continued support of our project and the Waksman Institute of Rutgers University for providing additional resources.</p>
         <h1>Discussions</h1>
-        <p>
-
-        </p>
+        <a href="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/view?usp=sharing" target="_blank">Click here to open our E-poster on google drive and provide comments and engage in discussions with one another!</a>
         <h1>About</h1>
         <p>
           This website is made to display our project on the role of oil exterminators in the environment. We are a group of students from Rutgers Preparatory School who are interested in learning more about the field of microbiology and have chosen this topic for our year round advanced research course taught by and guided by Ms. Wleklinski. Thank you for visiting!
