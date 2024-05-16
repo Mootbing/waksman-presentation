d7ecb2330620315d5deb5f42f9d24a09aa296494
Mootbing <50122069+Mootbing@users.noreply.github.com>
2024-05-16 00:52:51 -0400

    add methods video

--- waksman/src/App.js
+++ waksman/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <iframe style={{height: "80vh"}} src="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/preview" width="100%" height="100%" allow="autoplay"></iframe>
         <h1>Videos</h1>
         {/* intro */}
-        {/* methods */}
+        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/rjTokjiB0D4?si=8_qFy0mTZHlTJsap" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/3zYNcor2Soc?si=1sQbXEhzXScqkYl8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/FtVhqCX6Pfw?si=1kKgQK5xExqco0eR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         {/* bacillus */}
