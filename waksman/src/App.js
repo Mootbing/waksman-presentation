452b70145454c5b29dc314a61d929c1f1dc89538
Mootbing <50122069+Mootbing@users.noreply.github.com>
2024-05-17 05:59:26 -0400

    final

--- waksman/src/App.js
+++ waksman/src/App.js
@@ -10,25 +10,53 @@ function App() {
       </head>
 
       <body>
-        <h1>E-Poster</h1>
-        <iframe style={{height: "80vh"}} src="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/preview" width="100%" height="100%" allow="autoplay"></iframe>
-        <h1>Videos</h1>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/VKzgxltrBBg?si=6JrUj6bATnS8SvVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/rjTokjiB0D4?si=8_qFy0mTZHlTJsap" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/3zYNcor2Soc?si=1sQbXEhzXScqkYl8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/FtVhqCX6Pfw?si=1kKgQK5xExqco0eR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/_ubDPwv9yIg?si=9YLIU5DbHFIBKfNS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/5oEwQ401Ggw?si=4J-Tmkvu06pdE0sY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h1>Acknoweldgements</h1>
-        <p>We would like to thank Ms. Wleklinski, Dr. Pierce, and Ms. Zanfardino of Rutgers Preparatory School for their continued support of our project and the Waksman Institute of Rutgers University for providing additional resources.</p>
-        <div id="discussions">
-          <h1>Discussions</h1>
-          <a href="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/view?usp=sharing" target="_blank">Click here to open our E-poster on google drive and provide comments and engage in discussions with one another and original poster authors!</a>
-        </div>
-        <h1>About</h1>
-        <p>
-          This website is made to display our project on the role of oil exterminators in the environment. We are a group of students from Rutgers Preparatory School who are interested in learning more about the field of microbiology and have chosen this topic for our year round advanced research course taught by and guided by Ms. Wleklinski. Thank you for visiting!
-        </p>
+        <center>
+          <h1>Intro</h1>
+          <p>Welcome to our Advanced Waksman research project! The following is an introductory video made by the researchers who were involved.</p>
+          <iframe width="100%" height="300vw" style={{paddingBottom: 25}} src="https://www.youtube.com/embed/VKzgxltrBBg?si=6JrUj6bATnS8SvVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <h1>E-Poster</h1>
+          <p>Here is an online version of our Waksman poster. Feel free to <a href="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/view?usp=sharing" target="_blank">open it in a new tab</a>, explore around, or save it as a PDF!</p>
+          <iframe style={{height: "70vh"}} src="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/preview" width="100%" height="100%" allow="autoplay"></iframe>
+          <h1 style={{paddingTop: 25}}>Video Gallery</h1>
+          <p>Here's all the videos on our poster that are linked by QR codes with respect to each section.</p>
+          <h2>Methods</h2>
+          <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/rjTokjiB0D4?si=8_qFy0mTZHlTJsap" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <h2>Results</h2>
+          <h3>P.Putida & Cornflower</h3>
+          <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/3zYNcor2Soc?si=1sQbXEhzXScqkYl8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <h3>P.Putida & Motor</h3>
+          <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/FtVhqCX6Pfw?si=1kKgQK5xExqco0eR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <h3>B.Subtilis & Cornflower</h3>
+          <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/_ubDPwv9yIg?si=9YLIU5DbHFIBKfNS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <h3>C.Sporogenes & Both</h3>
+          <iframe width="100%" height="300vw" src="https://www.youtube.com/embed/5oEwQ401Ggw?si=4J-Tmkvu06pdE0sY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <div id="discussions" style={{paddingTop: 25}}>
+            <h1>Discussions</h1>
+            <p>A list of possible avenues to explore in future studies and points to kickoff a discussion</p>
+            <ul>
+              <li>Repeating the Bacillus experiment to confirm its bioremediative properties</li>
+              <li>Genetic modification of these bacteria could be explored to enhance their oil-degradation </li>
+              <li>Collecting more data over certain time intervals to ensure we get the whole picture of the bioremediative process</li>
+              <li>Future studies should investigate the bioremediation capabilities of these bacteria on a wider variety of oils, such as crude oil, used motor oil (as opposed to new), and other types of vegetable oils</li>
+              <li>Investigating the interactions between different bacterial strains in mixed cultures could provide insights into synergistic effects that enhance bioremediation</li>
+              <li>Laboratory conditions are controlled and may not fully replicate the complexities of natural environments. Perhaps conducting a field study of these bacteria in their natural environments will be benificial to understanding their properties</li>
+            </ul>
+            <a href="https://drive.google.com/file/d/1-uuHndjDpgM0UvXLHJPvG4g4h01Hp6le/view?usp=sharing" target="_blank">Participate in our live discussion board!</a>
+            <p><i>The discussion board is hosted on google drive and consists of adding to other people's comment threads or creating your own in a region of the poster you would like to comment on.</i></p>
+          </div>
+          <h1>Sources</h1>
+          <ul>
+            <li><a href="https://www.edvotek.com/956.110405.pdf">https://www.edvotek.com/956.110405.pdf</a></li>
+            <li><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4136109/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4136109/</a></li>
+            <li><a href="https://www.frontiersin.org/journals/microbiology/articles/10.3389/fmicb.2019.02033/full">https://www.frontiersin.org/journals/microbiology/articles/10.3389/fmicb.2019.02033/full</a></li>
+          </ul>
+          <h1>Acknolwedge</h1>
+          <p>We would like to thank Ms. Wleklinski, Dr. Pierce, and Ms. Zanfardino of Rutgers Preparatory School for their continued support of our project and the Waksman Institute of Rutgers University for providing additional resources.</p>
+          <h1>About</h1>
+          <p>
+            This website is made to display our project on the role of oil exterminators in the environment. We are a group of students from Rutgers Preparatory School who are interested in learning more about the field of microbiology and have chosen this topic for our year round advanced research course taught by and guided by Ms. Wleklinski. Thank you for visiting!
+          </p>
+        </center>
       </body> 
     </div>
   );
